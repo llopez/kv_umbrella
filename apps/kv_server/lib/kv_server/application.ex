1dc7b2be02d6975b47e4a4883385f81cc0020d51
Luis Lopez <luigibyte@gmail.com>
2019-10-04 13:12:44 -0300

    Add Task & TaskSupervisor

--- apps/kv_server/lib/kv_server/application.ex
+++ apps/kv_server/lib/kv_server/application.ex
@@ -6,9 +6,13 @@ defmodule KVServer.Application do
   use Application
 
   def start(_type, _args) do
+    port = String.to_integer(System.get_env("PORT") || "4040")
+
     children = [
       # Starts a worker by calling: KVServer.Worker.start_link(arg)
       # {KVServer.Worker, arg}
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      {Task, fn -> KVServer.accept(port) end}
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
